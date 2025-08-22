3421ec7d9db0391262940bec7527f6ff588181ea
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-22 18:54:43 +0300

    Fetch and display all movies from the database
    Added functionality to fetch and display all movies from the database.

--- Part C.py
+++ Part C.py
@@ -59,6 +59,7 @@ VALUES (?, ?, ?, ?, ?, ?);
 ''', data)
 
 # show all movies
+
 cursor.execute("SELECT * FROM movies")
 movies = cursor.fetchall()
 for movie in movies:
@@ -69,6 +70,7 @@ for movie in movies:
 conn.commit()
 
 # search movie script
+
 def search_movies():
     search_term = input("\nEnter a movie name or part of a movie name: ").strip()
 
