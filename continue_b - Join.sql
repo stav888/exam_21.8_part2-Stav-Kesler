4c7f5b54efd249fd1874a14b87da310bd73780d8
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-22 18:11:10 +0300

    Update continue_b - Join.sql

--- continue_b - Join.sql
+++ continue_b - Join.sql
@@ -109,3 +109,24 @@ WHERE id NOT IN (
     FROM tourists
     WHERE tour_id IS NOT NULL
 );
+
+#8
+SELECT
+    t.id AS tourist_id,
+    t.first_name,
+    t.last_name,
+    t.tour_id AS currently_assigned_trip,
+    c.country_name,
+    tr.id AS trip_id,
+    tr.tour_name,
+    tr.start_date,
+    tr.end_date
+FROM
+    tours tr
+CROSS JOIN
+    tourists t
+JOIN
+    countries c ON t.country_id = c.id
+ORDER BY
+    tr.id, t.id;
+
