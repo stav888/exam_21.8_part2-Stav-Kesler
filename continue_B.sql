144f06ad62120d4ad82252a8d80229f1c9aa0f73
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-22 11:11:48 +0300

    Update SQL queries for movie statistics

--- continue_B.sql
+++ continue_B.sql
@@ -1,4 +1,16 @@
-'
-select count(genre)
-from movies;
-'
+#1
+SELECT genre, COUNT(*) as movies_count
+FROM movies
+GROUP BY genre
+ORDER BY movies_count DESC;
+
+#2
+SELECT SUM(revenue) as all_revenue , year
+FROM movies
+GROUP BY year
+
+#3
+SELECT genre, AVG(revenue) as avg_revenue
+FROM movies
+GROUP BY genre
+ORDER BY avg_revenue DESC;
