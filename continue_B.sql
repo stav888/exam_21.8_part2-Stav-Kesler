80b07227626af3c7816326d98c21c06c57d7d277
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-22 15:28:32 +0300

    Update continue_B.sql

--- continue_B.sql
+++ continue_B.sql
@@ -14,3 +14,17 @@ SELECT genre, AVG(revenue) as avg_revenue
 FROM movies
 GROUP BY genre
 ORDER BY avg_revenue DESC;
+
+#4
+SELECT genre, AVG(revenue) as avrage_revenue, language
+FROM movies
+GROUP BY genre, language
+ORDER BY genre ASC;
+
+#5
+SELECT language, COUNT(*) as movie_count
+FROM movies
+GROUP BY language
+ORDER BY movie_count ASC
+
+#6
