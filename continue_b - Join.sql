147e2901a8f1a0168cd00ad0fd7c79eaf6f546fa
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-22 17:40:42 +0300

    Add SQL queries for tourists and tour updates
    Added queries to select tourists and update tour dates.

--- continue_b - Join.sql
+++ continue_b - Join.sql
@@ -63,5 +63,39 @@ WHERE t.tour_id IS NULL;
 DELETE FROM tourists
 WHERE tour_id IS NULL;
 
-#6
+SELECT t.first_name, t.last_name
+FROM tourists t
+WHERE t.tour_id IS NOT NULL;
 
+#6
+SELECT
+    tr.tour_name,
+    tr.description,
+    tr.start_date,
+    tr.end_date
+FROM tours tr
+LEFT JOIN tourists t ON tr.id = t.tour_id
+WHERE t.tour_id IS NULL;
+
+
+
+UPDATE tours
+SET
+    start_date = DATE(start_date, '+1 year'),
+    end_date = DATE(end_date, '+1 year')
+WHERE
+    id NOT IN (
+        SELECT DISTINCT tour_id
+        FROM tourists
+        WHERE tour_id IS NOT NULL
+    );
+
+
+SELECT
+    tr.tour_name,
+    tr.description,
+    tr.start_date,
+    tr.end_date
+FROM tours tr
+LEFT JOIN tourists t ON tr.id = t.tour_id
+WHERE t.tour_id IS NOT NULL
