037acb0d47d885a8ca96fcf886d92c45a9c19745
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-22 21:44:56 +0300

    Update continue_b - Join.sql

--- continue_b - Join.sql
+++ continue_b - Join.sql
@@ -1,5 +1,5 @@
 #1
-SELECT t.first_name, t.last_name, c.country_name, c.country_name
+SELECT t.first_name, t.last_name, c.country_name
 FROM tourists t
 INNER JOIN countries c ON c.id = t.country_id;
 
