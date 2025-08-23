f924e6421bf7d474f3321a43aa055c820cbd42d9
stav888 <44903234+stav888@users.noreply.github.com>
2025-08-23 09:27:35 +0300

    Update D – Bonus -2.sql

--- D – Bonus -2.sql
+++ D – Bonus -2.sql
@@ -1,29 +1,28 @@
 CREATE TABLE movies (
     id SERIAL PRIMARY KEY,
-    movie_name TEXT NOT NULL UNIQUE, -- Unique movie name
+    movie_name TEXT NOT NULL UNIQUE,
     genre TEXT NOT NULL,
     country TEXT NOT NULL,
     language TEXT NOT NULL,
-    year INTEGER NOT NULL CHECK (year >= 2009), -- Ensures movie is from the last 15 years
-    revenue REAL NOT NULL CHECK (revenue >= 0) -- Revenue in millions, cannot be negative
+    year INTEGER NOT NULL CHECK (year >= 2009),
+    revenue REAL NOT NULL CHECK (revenue >= 0)
 );
 
+DROP FUNCTION IF EXISTS total_revenue_by_year(INTEGER);
 
-DROP FUNCTION IF EXISTS total_revenue_by_year(p_year INTEGER)
-
-CREATE OR REPLACE FUNCTION total_revenue_by_year(p_year INTEGER)
+CREATE OR REPLACE FUNCTION total_revenue_by_year(movie_year INTEGER)
 RETURNS REAL
-DECLARE total_revenue REAL;
 LANGUAGE plpgsql AS
 $$
+DECLARE
+    total_revenue REAL;
 BEGIN
     SELECT SUM(revenue) INTO total_revenue
     FROM movies
-    WHERE year = p_year;
+    WHERE year = movie_year;
 
     RETURN total_revenue;
 END;
 $$;
-
 
 SELECT total_revenue_by_year(2023) AS total_revenue;
